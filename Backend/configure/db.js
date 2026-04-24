ae099040eb37d4ce5dcbedce7fdea5bf1d5fe829
SUBHAM PANDA <subhampanda.official@gmail.com>
2026-04-24 16:54:53 +0530

    fix mongodb issue

--- Backend/configure/db.js
+++ Backend/configure/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected ✅");
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB Connected");
   } catch (err) {
     console.log(err);
     process.exit(1);
