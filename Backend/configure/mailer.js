93316caef41ac7d2e16ee017906a963e57dfdd8c
SUBHAM PANDA <subhampanda.official@gmail.com>
2026-05-03 19:08:15 +0530

    fixed OTP issue

--- Backend/configure/mailer.js
+++ Backend/configure/mailer.js
@@ -1,14 +1,11 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: { rejectUnauthorized: false },
   pool: true,
   maxConnections: 3,
 });
